b65efdca0b390c5a1ae20e9be403a07eb4534803
Valentyn Digtiarenko <baggins999@gmail.com>
2022-06-06 14:26:19 +0300

    second

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import ImageGallery from './ImageGallery';
 import Button from './Button';
+import Loader from './Loader';
 
 // import PropTypes from 'prop-types';
 
@@ -16,12 +17,24 @@ export class App extends Component {
     status: 'idle',
   };
 
-  handleFetch = searchQuery => {
+  handleSearchQuery = searchQuery => {
+    this.setState({ searchQuery, page: 1 });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchQuery !== this.state.searchQuery) {
+      this.setState({ result: [] });
+      this.fetchImages();
+    }
+  }
+
+  fetchImages = () => {
+    const { searchQuery, page } = this.state;
     const Api = '27492943-191b6e85ce2b26a7ce823ae12';
-    this.setState({ status: 'pending', searchQuery });
+    this.setState({ status: 'pending' });
 
     return fetch(
-      `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${this.state.page}&per_page=12&key=${Api}`,
+      `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${page}&per_page=24&key=${Api}`,
     )
       .then(response => response.json())
       .then(data => {
@@ -48,19 +61,22 @@ export class App extends Component {
       }),
     );
 
-    this.setState({ result: normalizedHits, status: 'resolved' });
+    this.setState(prevState => ({
+      result: [...prevState.result, ...normalizedHits],
+      page: prevState.page + 1,
+      status: 'resolved',
+    }));
   };
 
   handleLoadMore = () => {
-    console.log('click');
-    this.setState(prevState => ({ ...prevState, page: prevState.page + 1 }));
-    this.handleFetch(this.state.searchQuery);
+    this.fetchImages();
   };
 
   render() {
     return (
       <div>
-        <SearchBar onSubmit={this.handleFetch} />
+        <SearchBar onSubmit={this.handleSearchQuery} />
+        {this.state.status === 'pending' && <Loader />}
         <ImageGallery pictures={this.state.result} />
         {this.state.result.length > 0 && (
           <Button onClick={this.handleLoadMore} />
